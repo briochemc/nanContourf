5558d5cc06bf8ea0827d205810ceed9838eeafb2
Benoit Pasquier <briochemc@gmail.com>
2021-07-08 10:11:26 +1000

    Fix docstring

--- nanContourf.m
+++ nanContourf.m
@@ -1,27 +1,32 @@
 
-function h = nanContourf(xb,yb,x,y,z,varargin)
+function h = nanContourf(xb, yb, x, y, z, varargin)
 %
-% h = nanContourf(xb,yb,x,y,z)
+% h = nanContourf(xb, yb, x, y, z)
 %
 % requires inpaint_nans.m (google it or MATLAB Central)
 %
 % Plots an extrapolated contourf of your (x,y,z), and uses
 % xb and yb (the boundaries of the boxes of your map)
-% to fill in the where the nans are. Among other things,
+% to fill in the nans. Among other things,
 % this function eventually calls
-%   h = contourf(x,y,z)
-% Default value for the contour linestyle has been
-% changed to'none' (because it is prettier I think)
-% default value for the fill linestyle is the default of fill,
-% and default value for the nan fill is a gray (rgb = .8*[1 1 1])
 %
-% Below is an example use. From the map of peaks,
-% to which I incorporated islands, and continents with lakes,
-% which are connected patches of nans.
-% Copy paste without the first percent sign, and run
-% to see a few different options.
+% h = contourf(x, y, z)
 %
-% % start from peaks, cropped for asymmetry
+% and you should be able to pass usual contourf arguments too,
+% as in, e.g.:
+%
+% h = nanContourf(xb, yb, x, y, z, levels)
+%
+% The handle returned by nanContourf is that of the inner call
+% to contourf.
+%
+% Default linestyle around nans is 'none' (prettier IMHO).
+% Default fill-color for nans is gray (rgb = .8*[1 1 1]).
+%
+% ---
+%
+% Example use
+%
 % z = peaks ;
 % z(end,:) = [] ;
 %
@@ -45,14 +50,15 @@ function h = nanContourf(xb,yb,x,y,z,varargin)
 % xb = [2*x(1)-xb(1) xb 2*x(end)-xb(end)] ;
 % yb = (y(2:end)+y(1:end-1))/2 ;
 % yb = [2*y(1)-yb(1) yb 2*y(end)-yb(end)] ;
-% nanContourf_v3(xb,yb,x,y,z)
+% nanContourf(xb,yb,x,y,z)
 % title('nanContourf')
-% filling the nans for the contours to connect nicely
-% with the borders
+
+% filling the nans with inpaint_nans to prevent the contour
+% lines from stopping before the borders around the nans.
 z_ext = inpaint_nans(z) ;
 h = contourf(x,y,z_ext,varargin{:},'linestyle','none') ;
 hold on
-% black and white z
+% black and white z for filling nans with gray color
 z_bw = zeros(size(z)) ;
 z_bw(find(isnan(z))) = 1 ;
 % gray color to fill the nans ; can be changed
